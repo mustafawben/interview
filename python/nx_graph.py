20e32f505058d84aef505bb0430beb7db080ec1f
mustafawben <mustafawben@gmail.com>
2018-01-13 18:07:10 -0500

    Update nx_graph.py

--- python/nx_graph.py
+++ python/nx_graph.py
@@ -11,4 +11,11 @@ relationships = [
     {"parent": "dean", "child": "edgar"}
 ]
 
+import networkx as nx
+
+G = nx.Graph()
+edge_list = [("alice","bob") , ("bob", "charlie") , ("dean", "edgar")]
+G.add_edges_from(edge_list)
+print ("Betweenness Centrality: ", nx.betweenness_centrality(G))
+
 # TODO: build a graph, compute the betweeness centrality
